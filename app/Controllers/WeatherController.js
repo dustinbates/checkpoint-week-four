2eed78adef3ce77e05f78df83bf340840323d4aa
Dustin Bates <dustin.bates98@gmail.com>
2023-02-12 12:58:47 -0700

    weather converts from F and C onclick

--- app/Controllers/WeatherController.js
+++ app/Controllers/WeatherController.js
@@ -1,10 +1,17 @@
 import { appState } from "../AppState.js";
 import { weatherService } from "../Services/WeatherService.js";
 import { Pop } from "../Utils/Pop.js";
-import { setHTML } from "../Utils/Writer.js";
+import { setHTML, setText } from "../Utils/Writer.js";
 
 function _drawWeather(){
     setHTML('weather', appState.weather.WeatherTemplate)
+
+    if (appState.weather.isF == true){
+        setHTML('temp-type', `<i class="mdi mdi-temperature-fahrenheit fs-3"></i>`)
+    }
+    else{
+        setHTML('temp-type', `<i class="mdi mdi-temperature-celsius fs-3"></i>`)
+    }
 }
 
 export class WeatherController {
@@ -20,5 +27,11 @@ export class WeatherController {
             Pop.error(error)
             console.error(error.message)
         }
+    }
+
+    changeTemp(){
+        appState.weather.isF = !appState.weather.isF
+        appState.weather = appState.weather
+        
     }
 }
--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -2,13 +2,21 @@ export class Weather {
     constructor(data){
         this.name = data.name
         this.temp = data.main.temp
-        
+        this.tempC = Math.floor(data.main.temp) - 273
+        this.tempF = Math.floor(this.tempC * 9/5) + 32
+        this.isF = true
     }
 
     get WeatherTemplate(){
-        return  `
-        <p>${this.temp}</p>
-        <p>${this.name}</p>
+        const temp = this.isF ? this.tempF : this.tempC
+        return /*html*/ `
+        <div class="d-block mx-2 my-3 " onclick="app.weatherController.changeTemp()">
+            <span class="d-flex flex-row">
+                <h2 id="temp">${temp}</h2>
+                <p id="temp-type"></p>
+            </span>
+            <h5 class="">${this.name}</h5>
+        </div>
         `
     }
 }
